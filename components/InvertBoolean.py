667471f50c878db738d9e37a3a18004ab66a8034
Uwe Kamper <me@uwekamper.de>
2017-06-21 21:16:13 +0200

    testing, one two

--- components/InvertBoolean.py
+++ components/InvertBoolean.py
@@ -4,9 +4,10 @@ import msgflo
 class InvertBoolean(msgflo.Participant):
   def __init__(self, role):
     d = {
-      'component': 'msgflo-project/InvertBoolean',
+      'component': 'ukflo/InvertBoolean',
       'label': 'Invert a boolean value',
       'icon': 'star-o',
+      
       'inports': [
         { 'id': 'in', 'type': 'boolean' },
       ],
